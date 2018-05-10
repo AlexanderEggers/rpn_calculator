b01b7c3931fa9ce4cdd3cf08918904dfe094f124
Mordag <alexander.eggers91@gmail.com>
2018-05-10 21:44:25 +1000

    Tweaking documentation

--- src/main/kotlin/org/demo/calculator/opertator/DivisionOperation.kt
+++ src/main/kotlin/org/demo/calculator/opertator/DivisionOperation.kt
@@ -9,7 +9,7 @@ class DivisionOperation : Operation(OperationType.DIVISION) {
 
     override fun onExecute(stack: LinkedList<Double>, values: Array<Double>): Boolean {
         /**
-         * Checks if the second operation value not null, which would return a NaN.
+         * Checks if the second operation value equals the value zero, which would return a NaN.
          */
         if (values[1] != 0.0) {
             stack.addLast(values[0].div(values[1]))
--- src/main/kotlin/org/demo/calculator/opertator/StackOperation.kt
+++ src/main/kotlin/org/demo/calculator/opertator/StackOperation.kt
@@ -2,6 +2,10 @@ package org.demo.calculator.opertator
 
 import java.util.*
 
+/**
+ * Operation which is used to insert new elements to the stack. This operation type requires no elements from the
+ * stack in order to work but can be reverted if needed.
+ */
 class StackOperation(private val value: String) : Operation(OperationType.STACK) {
 
     override fun onExecute(stack: LinkedList<Double>, values: Array<Double>): Boolean {
